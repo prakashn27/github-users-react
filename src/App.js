dfeee1289a46ff4708ab4c5f462021e266e05cb1
prakashn27 <nprakashece@gmail.com>
2017-10-08 15:52:44 -0400

    change styles for header

--- src/App.js
+++ src/App.js
@@ -4,14 +4,9 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Github username list Using React</h1>
-        </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-      </div>
     );
   }
 }
